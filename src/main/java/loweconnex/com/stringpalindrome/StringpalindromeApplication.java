15a500945ffd1895a70f52b6bcdf94e2c2369c1d
Paul Allen <allen-p11@ulster.ac.uk>
2023-12-10 00:20:04 +0000

    first commit with build script

---
+++ src/main/java/loweconnex/com/stringpalindrome/StringpalindromeApplication.java
@@ -0,0 +1,14 @@
+package loweconnex.com.stringpalindrome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class StringpalindromeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringpalindromeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loweconnex/com/stringpalindrome/StringpalindromeApplicationTests.java
@@ -0,0 +1,13 @@
+package loweconnex.com.stringpalindrome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringpalindromeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
